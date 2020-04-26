a2dcc766641e9b205957bc55ca68a11d454fc06d
wonszbagienny <matkub19@gmail.com>
2020-04-27 01:59:00 +0200

    eh

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def login(response: Response, credentials: HTTPBasicCredentials = Depends(securi
     session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
     app.tokens.append(session_token)
     response.set_cookie(key = "session_token", value = session_token)
-    response.headers["Location"] = "/welcome"
-    response.status_code = status.HTTP_302_FOUND 
-    #return RedirectResponse(url = '/welcome')
+    #response.headers["Location"] = "/welcome"
+    #response.status_code = status.HTTP_302_FOUND 
+    return RedirectResponse('/welcome')
 
 @app.post("/logout")
 def logout(response: Response, session_token: str = Cookie(None)):
