7648375e952443c0207fed62c830e09ffa436fd1
wonszbagienny <matkub19@gmail.com>
2020-05-11 02:53:52 +0200

    4th

--- routers/tracks.py
+++ routers/tracks.py
@@ -27,7 +27,7 @@ class Track(BaseModel):
     Bytes: int
     UnitPrice: float
 
-@router.get("/tracks", response_model=List[Track])
+@router.get("/tracks")
 async def tracks(page: int = 0, per_page: int = 10):
     router.db_connection.row_factory = aiosqlite.Row
     cursor = await router.db_connection.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT ? OFFSET ?;", (per_page, page * per_page))
