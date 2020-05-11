3db8248800695412fa0ab92bc9584d04845854d4
wonszbagienny <matkub19@gmail.com>
2020-05-11 04:21:51 +0200

    4th

--- routers/tracks.py
+++ routers/tracks.py
@@ -60,7 +60,7 @@ async def post_album(request: Album):
         raise HTTPException(status_code=404, detail={"error": "artist_id not found"})
     cursor = await router.db_connection.execute("INSERT INTO albums (Title, ArtistId) VALUES (?, ?);", (request.title, request.artist_id))
     await router.db_connection.commit()
-    return AlbumResponse(AlbumId = cursor.lastrowid, Title = request.title, ArtistID = request.artist_id)
+    return AlbumResponse(AlbumId = cursor.lastrowid, Title = request.title, ArtistId = request.artist_id)
 
 @router.get("/albums/{album_id}", status_code=200)
 async def get_album(album_id: int):
